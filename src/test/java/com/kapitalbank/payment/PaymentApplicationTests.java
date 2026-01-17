ecdb8c9198dfb7facfd3cdfd481c880bfc7224e7
namrahov <nurlanemrahov90@gmail.com>
2026-01-17 10:43:38 +0400

    first commit

---
+++ src/test/java/com/kapitalbank/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapitalbank.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
